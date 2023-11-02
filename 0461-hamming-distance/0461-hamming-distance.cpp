3b1eac0b8d8fc85e8a493c60e4c432fef10fb49d
Nimish Singh  Chouhan <81174768+NimishSinghChouhan@users.noreply.github.com>
2023-11-02 15:02:43 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (29.92%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int diff = x^y;
+        
+        int ans = 0;
+        
+        while(diff){
+            ans += (diff&1);
+            diff >>= 1;
+        }
+        
+        return ans;
+    }
+};
