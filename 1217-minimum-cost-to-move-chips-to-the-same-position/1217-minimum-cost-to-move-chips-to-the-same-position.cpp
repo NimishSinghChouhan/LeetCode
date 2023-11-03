87ff2d66007da128d9bcf93f15944b70bdc90886
Nimish Singh  Chouhan <81174768+NimishSinghChouhan@users.noreply.github.com>
2023-11-03 11:52:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (16.06%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& chips) {
+        int odd_count = 0, even_count = 0;
+        for(int chip : chips){
+            if(chip % 2 == 1) odd_count++;
+            else even_count++;
+        }
+        //assume we have change all odd chip to 1 and all even chip to 0
+        //now we want to either change all 0 to 1 or all 1 to 1
+        //we choose to change all 0 to 1 if 0's count is less and vice versa
+        return min(even_count, odd_count);
+    }
+};
