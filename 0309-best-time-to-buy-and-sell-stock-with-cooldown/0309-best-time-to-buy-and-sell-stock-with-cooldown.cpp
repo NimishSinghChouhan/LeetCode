2d65d13d2ae52cf921599239885653e4332b9f3e
Nimish Singh  Chouhan <81174768+NimishSinghChouhan@users.noreply.github.com>
2023-11-14 12:13:23 +0530

    Time: 475 ms (5.01%), Space: 168 MB (5.00%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        
+        if(n == 0) return 0;
+        
+        //pad 1 transactions, pad 2 days
+        vector<vector<int>> dp(n+1, vector(n+2, 0));
+        
+        for(int i = 1; i <= n; i++){ //transactions
+            vector<int> hold(n+2, INT_MIN);
+            for(int j = 2; j < n+2; j++){ //days
+                dp[i][j] = max(dp[i][j-1], prices[j-2]+hold[j-1]);
+                hold[j] = max(hold[j-1], dp[i-1][j-2] - prices[j-2]);
+            }
+//             cout << "dp" << endl;
+//             for(int j = 2; j < n+2; j++){
+//                 cout << dp[i][j] << " ";
+//             }
+//             cout << endl;
+            
+//             cout << "hold" << endl;
+//             for(int j = 2; j < n+2; j++){
+//                 cout << hold[j] << " ";
+//             }
+//             cout << endl;
+            
+//             cout << "--------" << endl;
+        }
+        
+        return dp[n][n+1];
+    }
+};
